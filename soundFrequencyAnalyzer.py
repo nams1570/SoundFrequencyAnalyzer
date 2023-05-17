220724b0a6cb03508551079c593ae2d4991eed9a
Manasvini Narayanan <write2manasvini@gmail.com>
2023-05-17 15:17:33 -0700

    program completed, added ascii art

--- soundFrequencyAnalyzer.py
+++ soundFrequencyAnalyzer.py
@@ -7,17 +7,84 @@ import matplotlib.pyplot as plt
 
 #Taking input from microphone
 #Same program as before
-fs = 44100 #sampling frquency 
-duration = 5
-channels = 1
-print("Recording for {} seconds".format(duration))
-audio_data = sd.rec(int(duration * fs), samplerate=fs, channels=channels)
+print("Welcome to the sound frequency analyzer!")
+asciiart = """
 
-sd.wait()
-audio = np.squeeze(audio_data)
-print(audio_data)
+   _____                       __
+  / ___/____  __  ______  ____/ /
+  \__ \/ __ \/ / / / __ \/ __  / 
+ ___/ / /_/ / /_/ / / / / /_/ /  
+/____/\____/\__,_/_/ /_/\__,_/   
+                                 
+"""
+print(asciiart)
+asciiartF  = """
 
-wavfile.write("test.wav", fs, audio)
+    ______                                           
+   / ____/_______  ____ ___  _____  ____  _______  __
+  / /_  / ___/ _ \/ __ `/ / / / _ \/ __ \/ ___/ / / /
+ / __/ / /  /  __/ /_/ / /_/ /  __/ / / / /__/ /_/ / 
+/_/   /_/   \___/\__, /\__,_/\___/_/ /_/\___/\__, /  
+                   /_/                      /____/   
+"""
+print(asciiartF)
+asciiartA = """
 
-fourier = np.fft.fft(audio_data)
-print(fourier)
+    ___                __                     
+   /   |  ____  ____ _/ /_  ______  ___  _____
+  / /| | / __ \/ __ `/ / / / /_  / / _ \/ ___/
+ / ___ |/ / / / /_/ / / /_/ / / /_/  __/ /    
+/_/  |_/_/ /_/\__,_/_/\__, / /___/\___/_/     
+                     /____/                   
+"""
+print(asciiartA)
+asciiart2 = """
+ ___________________________________________
+|  _______________________________________  |
+| / .-----------------------------------. \ |
+| | | /\ :                        90 min| | |
+| | |/--\:....................... NR [ ]| | |
+| | `-----------------------------------' | |
+| |      //-\\   |         |   //-\\      | |
+| |     ||( )||  |_________|  ||( )||     | |
+| |      \\-//   :....:....:   \\-//      | |
+| |       _ _ ._  _ _ .__|_ _.._  _       | |
+| |      (_(_)| |(_(/_|  |_(_||_)(/_      | |
+| |               low noise   |           | |
+| `______ ____________________ ____ ______' |
+|        /    []             []    \        |
+|       /  ()                   ()  \       |
+!______/_____________________________\______!
+Art by Simon Williams
+"""
+print(asciiart2)
+print("Would you like to ready to record? Type any letter when you are ready and 'q' to quit")
+userInput = input()
+while(userInput != "q"):
+    fs = 44100 #sampling frquency 
+    duration = 5
+    channels = 1
+    print("Recording for {} seconds".format(duration))
+    audio_data = sd.rec(int(duration * fs), samplerate=fs, channels=channels)
+    sd.wait()
+    audio = np.squeeze(audio_data)
+    print(audio_data)
+    wavfile.write("test.wav", fs, audio)
+    print("Recording saved as test.wav")
+    fourier = np.fft.fft(audio_data)
+# Each element in fourier represents the amplitude and phase information for a specific frequency component. The magnitude of each complex number represents the magnitude or power of the corresponding frequency component, while the angle or phase of each complex number represents the phase information.
+#print(fourier)
+
+#Plotting the data
+    print("Thank you for your sound input! Here is the frequency analysis of your sound")
+    print("Plotting...")
+    sd.wait(3)
+    print("Give us a second..")
+    frequencyDomain = np.linspace(0, fs, len(fourier))
+    plt.plot(frequencyDomain, fourier, color='green')
+    plt.savefig("fourier.png")
+    plt.show()
+    print("Plot saved as fourier.png")
+    print("Would you like to ready to record again? Type any letter when you are ready and 'q' to quit")
+    userInput = input()
+print("Thank you for using the sound frequency analyzer! Goodbye!")
