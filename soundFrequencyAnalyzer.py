b76d1ac56af2abbc31f3eab379f91c87448c3c1d
Manasvini Narayanan <write2manasvini@gmail.com>
2023-12-17 14:52:52 -0800

    used the method

--- soundFrequencyAnalyzer.py
+++ soundFrequencyAnalyzer.py
@@ -12,59 +12,10 @@ def convertToFreqDomain(audio:np.ndarray)->np.ndarray:
 
 #Taking input from microphone
 #Same program as before
-print("Welcome to the sound frequency analyzer!")
-asciiart = """
 
-   _____                       __
-  / ___/____  __  ______  ____/ /
-  \__ \/ __ \/ / / / __ \/ __  / 
- ___/ / /_/ / /_/ / / / / /_/ /  
-/____/\____/\__,_/_/ /_/\__,_/   
-                                 
-"""
-print(asciiart)
-asciiartF  = """
-
-    ______                                           
-   / ____/_______  ____ ___  _____  ____  _______  __
-  / /_  / ___/ _ \/ __ `/ / / / _ \/ __ \/ ___/ / / /
- / __/ / /  /  __/ /_/ / /_/ /  __/ / / / /__/ /_/ / 
-/_/   /_/   \___/\__, /\__,_/\___/_/ /_/\___/\__, /  
-                   /_/                      /____/   
-"""
-print(asciiartF)
-asciiartA = """
-
-    ___                __                     
-   /   |  ____  ____ _/ /_  ______  ___  _____
-  / /| | / __ \/ __ `/ / / / /_  / / _ \/ ___/
- / ___ |/ / / / /_/ / / /_/ / / /_/  __/ /    
-/_/  |_/_/ /_/\__,_/_/\__, / /___/\___/_/     
-                     /____/                   
-"""
-print(asciiartA)
-asciiart2 = """
- ___________________________________________
-|  _______________________________________  |
-| / .-----------------------------------. \ |
-| | | /\ :                        90 min| | |
-| | |/--\:....................... NR [ ]| | |
-| | `-----------------------------------' | |
-| |      //-\\   |         |   //-\\      | |
-| |     ||( )||  |_________|  ||( )||     | |
-| |      \\-//   :....:....:   \\-//      | |
-| |       _ _ ._  _ _ .__|_ _.._  _       | |
-| |      (_(_)| |(_(/_|  |_(_||_)(/_      | |
-| |               low noise   |           | |
-| `______ ____________________ ____ ______' |
-|        /    []             []    \        |
-|       /  ()                   ()  \       |
-!______/_____________________________\______!
-Art by Simon Williams
-"""
-print(asciiart2)
 print("Would you like to ready to record? Type any letter when you are ready and 'q' to quit")
 userInput = input()
+
 while userInput!= "q":
     fs = 44100  # sampling frequency
     duration = 5  # duration in seconds
@@ -79,7 +30,7 @@ while userInput!= "q":
     wavfile.write("test.wav", fs, audio)
     print("Recording saved as test.wav")
 
-    fourier = np.fft.fft(audio_data[:, 0])
+    fourier = convertToFreqDomain(audio_data[:,0])
 
     # Plotting the data
     print("Thank you for your sound input! Here is the frequency analysis of your sound")
