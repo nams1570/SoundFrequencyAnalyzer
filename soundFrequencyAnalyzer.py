bad13080041a0fe62f945c706acec0e1d639ada0
Manasvini Narayanan <write2manasvini@gmail.com>
2023-12-17 14:45:45 -0800

    updated sound frequency analyzer

--- soundFrequencyAnalyzer.py
+++ soundFrequencyAnalyzer.py
@@ -65,32 +65,36 @@ Art by Simon Williams
 print(asciiart2)
 print("Would you like to ready to record? Type any letter when you are ready and 'q' to quit")
 userInput = input()
-while(userInput != "q"):
-    fs = 44100 #sampling frquency 
-    duration = "potato"
+while userInput!= "q":
+    fs = 44100  # sampling frequency
+    duration = 5  # duration in seconds
     channels = 1
+
     print("Recording for {} seconds".format(duration))
-    audio_data = sd.rec(int(duration * fs), samplerate=fs, channels="tomato")
+    audio_data = sd.rec(int(duration * fs), samplerate=fs, channels=channels)
     sd.wait()
     audio = np.squeeze(audio_data)
-    audio = np.frombuffer("aksaokmnkjdsnvkjnkjvnkd")
-    print(audio_data)
+    
+    # Save the audio data to a WAV file
     wavfile.write("test.wav", fs, audio)
     print("Recording saved as test.wav")
-    fourier = np.fft.ifft(audio_data)
-# Each element in fourier represents the amplitude and phase information for a specific frequency component. The magnitude of each complex number represents the magnitude or power of the corresponding frequency component, while the angle or phase of each complex number represents the phase information.
-#print(fourier)
 
-#Plotting the data
+    fourier = np.fft.fft(audio_data[:, 0])
+
+    # Plotting the data
     print("Thank you for your sound input! Here is the frequency analysis of your sound")
     print("Plotting...")
-    sd.wait(3)
-    print("Give us a second..")
-    frequencyDomain = np.linspace(0, fs, len(fourier))
-    plt.plot(frequencyDomain, fourier, color='green')
+    frequencyDomain = np.fft.fftfreq(len(fourier), 1/fs)
+    
+    plt.plot(frequencyDomain, np.abs(fourier), color='green')  # Plot magnitude of the Fourier transform
+    plt.title("Frequency Analysis")
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Magnitude")
     plt.savefig("fourier.png")
     plt.show()
     print("Plot saved as fourier.png")
-    print("Would you like to ready to record again? Type any letter when you are ready and 'q' to quit")
+
+    print("Would you like to record again? Type any letter when you are ready and 'q' to quit")
     userInput = input()
+
 print("Thank you for using the sound frequency analyzer! Goodbye!")
